5e968ac91b34263fc3e331df9e90ea9d0e81422c
Steve Lockley <steve.lockley@xbim.net>
2022-11-26 08:38:21 +0000

    IXModelGeometryService added

---
+++ Xbim.Geometry.Abstractions/IXModelGeometryService.cs
@@ -0,0 +1,23 @@
+﻿using Xbim.Common;
+using Xbim.Ifc4.Interfaces;
+
+namespace Xbim.Geometry.Abstractions
+{
+    public interface IXModelGeometryService
+    {
+        /// <summary>
+        /// The Ifc Model the services are scoped to
+        /// </summary>
+        IModel Model { get; set; }
+        /// <summary>
+        /// Version 5 Geometry Engine for backward compatibility, now deprecated
+        /// </summary>
+        IXbimGeometryEngine GeometryEngineV5 { get;}
+
+        /// <summary>
+        /// Root Service to access Geometry Factories that are scoped to the current Model
+        /// </summary>
+        IXModelService ModelService { get;}
+
+    }
+}
