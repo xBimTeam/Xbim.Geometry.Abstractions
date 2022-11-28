c992aca64a0e14d724fca82b3e00e71254f2335b
Steve Lockley <steve.lockley@xbim.net>
2022-11-28 17:23:18 +0000

    ModelService Property of IXModelScoped renamed to ModelGeometryService

--- Xbim.Geometry.Abstractions/IXModelScoped.cs
+++ Xbim.Geometry.Abstractions/IXModelScoped.cs
@@ -2,7 +2,7 @@
 {
     public interface IXModelScoped
     {      
-        IXModelGeometryService ModelService { get; }
+        IXModelGeometryService ModelGeometryService { get; }
         IXLoggingService LoggingService { get; }
     }
 }
