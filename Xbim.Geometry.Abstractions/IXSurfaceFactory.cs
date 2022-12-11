ce105331d5f7adaccf7055851b9da67654ca62c3
Steve Lockley <steve.lockley@xbim.net>
2022-12-11 12:19:03 +0000

    Surface Factory interface added Build method

--- Xbim.Geometry.Abstractions/IXSurfaceFactory.cs
+++ Xbim.Geometry.Abstractions/IXSurfaceFactory.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xbim.Ifc2x3.Interfaces;
 
 namespace Xbim.Geometry.Abstractions
 {
     public interface  IXSurfaceFactory:IXModelScoped
     {
+        IXSurface Build(IIfcSurface surface);
         IXPlane BuildPlane(IXPoint origin, IXDirection normal);
     }
 }
