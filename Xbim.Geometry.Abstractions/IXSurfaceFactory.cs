e66a369186dd1be91afb8ed39751055ca38f75bf
Steve Lockley <steve.lockley@xbim.net>
2022-12-11 12:19:28 +0000

    Surface Factory updated

--- Xbim.Geometry.Abstractions/IXSurfaceFactory.cs
+++ Xbim.Geometry.Abstractions/IXSurfaceFactory.cs
@@ -5,7 +5,7 @@ using Xbim.Ifc2x3.Interfaces;
 
 namespace Xbim.Geometry.Abstractions
 {
-    public interface  IXSurfaceFactory:IXModelScoped
+    public interface IXSurfaceFactory : IXModelScoped
     {
         IXSurface Build(IIfcSurface surface);
         IXPlane BuildPlane(IXPoint origin, IXDirection normal);
