64dc34f18f09c2d61adae284a897c12366d37a33
Steve Lockley <steve.lockley@xbim.net>
2022-12-11 10:47:41 +0000

    Rectangle Trimmed Surface intrafce simplified

--- Xbim.Geometry.Abstractions/IXRectangleTrimmedSurface.cs
+++ Xbim.Geometry.Abstractions/IXRectangleTrimmedSurface.cs
@@ -3,12 +3,6 @@
     public interface IXRectangleTrimmedSurface : IXBoundedSurface
     {
         IXSurface BasisSurface { get; }
-        double U1 { get; }
-        double U2 { get; }
-        double V1 { get; }
-        double V2 { get; }
-        bool USense { get; }
-        bool VSense { get; }
-        bool IsPeriodic { get; }
+        void Bounds(ref double u1 , ref double u2, ref double v1, ref double v2); 
     }
 }
