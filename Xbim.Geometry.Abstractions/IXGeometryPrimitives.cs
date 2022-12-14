265ba780b0bb54eb538fc590b8f719caefa473cd
Steve Lockley <steve.lockley@xbim.net>
2022-12-14 10:09:12 +0000

    Geoemtry Primitive interface added

---
+++ Xbim.Geometry.Abstractions/IXGeometryPrimitives.cs
@@ -0,0 +1,11 @@
+﻿namespace Xbim.Geometry.Abstractions
+{
+    public interface IXGeometryPrimitives
+    {
+        IXPoint BuildPoint3d(double x, double y, double z);
+        IXPoint BuildPoint2d(double x, double y);
+        IXDirection BuildDirection3d(double x, double y, double z);
+        IXDirection BuildDirection2d(double x, double y);
+        IXLocation BuildLocation(double tx, double ty, double tz, double sc, double qw, double qx, double qy, double qz);
+    }
+}
