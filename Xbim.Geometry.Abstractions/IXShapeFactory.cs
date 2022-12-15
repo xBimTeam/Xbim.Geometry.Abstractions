df4d69d104d18c57180f1920e35cd1159b62f67e
Steve Lockley <steve.lockley@xbim.net>
2022-12-15 15:54:51 +0000

    MappedShape interface added

--- Xbim.Geometry.Abstractions/IXShapeFactory.cs
+++ Xbim.Geometry.Abstractions/IXShapeFactory.cs
@@ -35,5 +35,7 @@ namespace Xbim.Geometry.Abstractions
 		byte[] CreateWexBimMesh(IEnumerable<IXFace> faces, IXMeshFactors meshFactors);
 		byte[] CreateWexBimMesh(IXShape shape, IXMeshFactors meshFactors, ref bool hasCurves);
 		byte[] CreateWexBimMesh(IEnumerable<IXFace> faces, IXMeshFactors meshFactors, ref bool hasCurves);
-	}
+		IXShape BuildMappedShape(IIfcCartesianTransformationOperator transform, IIfcAxis2Placement origin, IXShape shape, out IXLocation location);
+
+    }
 }
