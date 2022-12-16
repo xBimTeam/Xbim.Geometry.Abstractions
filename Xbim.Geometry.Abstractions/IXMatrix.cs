aab43218a1103c2ef15d4ca4d3a7e527c21f9e56
Steve Lockley <steve.lockley@xbim.net>
2022-12-16 07:07:37 +0000

    Scal properties added to IXMatrix

--- Xbim.Geometry.Abstractions/IXMatrix.cs
+++ Xbim.Geometry.Abstractions/IXMatrix.cs
@@ -14,15 +14,15 @@ namespace Xbim.Geometry.Abstractions
         double M11 { get; set; }
         double M12 { get; set; }
         double M13 { get; set; }
-        double M14 { get; set; }
+        double ScaleX { get; set; }
         double M21 { get; set; }
         double M22 { get; set; }
         double M23 { get; set; }
-        double M24 { get; set; }
+        double ScaleY { get; set; }
         double M31 { get; set; }
         double M32 { get; set; }
         double M33 { get; set; }
-        double M34 { get; set; }
+        double ScaleZ { get; set; }
         double M44 { get; set; }
         double OffsetX { get; set; }
         double OffsetY { get; set; }
