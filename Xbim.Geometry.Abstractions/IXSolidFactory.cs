b8e297a0b05dcf47f2f1e520565899cde4b0e72e
Steve Lockley <steve.lockley@xbim.net>
2022-12-18 13:28:02 +0000

    IXSolidFactory interface extended to support Build of IIfcExtrudedAreaSolid

--- Xbim.Geometry.Abstractions/IXSolidFactory.cs
+++ Xbim.Geometry.Abstractions/IXSolidFactory.cs
@@ -8,6 +8,7 @@ namespace Xbim.Geometry.Abstractions
         IXShape Build(IIfcFacetedBrep ifcBrep);
         IXShape Build(IIfcFaceBasedSurfaceModel ifcSurfaceModel);
         IXSolid Build(IIfcCsgPrimitive3D ifcCsgPrimitive);
+        IXSolid Build(IIfcExtrudedAreaSolid ifcExtrudedAreaSolid);
 
     }
 }
