a902569c1c12784ff61cc1cac5bb2482aa715b87
Steve Lockley <steve.lockley@xbim.net>
2022-12-23 14:21:10 +0000

    IXShapeItem made immutable

--- Xbim.Geometry.Abstractions/IXShapeItem.cs
+++ Xbim.Geometry.Abstractions/IXShapeItem.cs
@@ -2,9 +2,9 @@
 {
     public interface IXShapeItem
     {
-        int Id { get; set; }
+        int Id { get; }
         
-        NodeType NodeType { get; set; }
+        NodeType NodeType { get;  }
        
     }
 }
