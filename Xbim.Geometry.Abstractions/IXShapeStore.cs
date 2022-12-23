95a251f3cdd986230d55b160131e2306a14fe30c
Steve Lockley <steve.lockley@xbim.net>
2022-12-23 17:39:49 +0000

    Colour support added to shapestore referencing

--- Xbim.Geometry.Abstractions/IXShapeStore.cs
+++ Xbim.Geometry.Abstractions/IXShapeStore.cs
@@ -67,7 +67,7 @@ namespace Xbim.Geometry.Abstractions
         /// <param name="child"></param>
         /// <param name="location"></param>
         /// <returns></returns>
-        IXShapeInstance AddReference(IXShapeAssembly parent, IXShapeComponent child, IXLocation location = null, IXMatrix transform = null, IXShapeMaterialItem material = null);
+        IXShapeInstance AddReference(IXShapeAssembly parent, IXShapeComponent child, IXLocation location = null, IXMatrix transform = null, IXShapeMaterialItem material = null, IXShapeColour colour=null);
 
         /// <summary>
         /// Returns a list of shapes with their locations for the specified assembly, if assembly is null all located shapes are returned in the store
