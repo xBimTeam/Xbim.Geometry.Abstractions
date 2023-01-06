73db926007f9cb20153220f2249dfc616701841c
Will Olner <will.olner@xbim.net>
2023-01-06 11:45:22 +0000

    updated default node type value

--- Xbim.Geometry.Abstractions/NodeType.cs
+++ Xbim.Geometry.Abstractions/NodeType.cs
@@ -2,15 +2,16 @@
 {
     public enum NodeType : byte
     {
-        Product = 0,
-        ProductRepresentation = 1,
-        ShapeRepresentation = 2,
-        BRepShape = 3,
-        FeatureElement = 4,
-        Colour = 5,
-        BRepShapeWithFeatures = 6,
-        LayeredMaterial = 7,
-        Material = 8,
-        MaterialConstituent = 9
+        Default = 0,
+        Product,
+        ProductRepresentation,
+        ShapeRepresentation,
+        BRepShape,
+        FeatureElement,
+        Colour,
+        BRepShapeWithFeatures,
+        LayeredMaterial,
+        Material,
+        MaterialConstituent
     }
 }
