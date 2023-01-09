ec6cee15f1d05b1c4cc7aefe2ddc9b56209a165b
Steve Lockley <steve.lockley@xbim.net>
2023-01-09 08:29:13 +0000

    SetTranslation of IXLocation updated

--- Xbim.Geometry.Abstractions/IXLocation.cs
+++ Xbim.Geometry.Abstractions/IXLocation.cs
@@ -3,12 +3,12 @@
     public interface IXLocation: IXMatrix
     {
         double Scale { get; }
-        IXPoint Translation { get; set; }
+        IXPoint Translation { get;  }
         IXQuaternion Rotation { get; }
         IXLocation Multiplied(IXLocation location);
         
         IXLocation Inverted();
         IXLocation ScaledBy(double scaleFactor);
-      
+        IXPoint SetTranslation(double x, double y, double z);
     }
 }
