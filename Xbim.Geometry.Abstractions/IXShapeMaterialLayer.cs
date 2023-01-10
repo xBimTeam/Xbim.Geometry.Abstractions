dc39fcc6da95fd2503bd1bb09b3cab8592ae6c23
Steve Lockley <steve.lockley@xbim.net>
2023-01-10 09:48:46 +0000

    Priority and IsVentilated added to IXShapeMaterialLayer

--- Xbim.Geometry.Abstractions/IXShapeMaterialLayer.cs
+++ Xbim.Geometry.Abstractions/IXShapeMaterialLayer.cs
@@ -5,5 +5,7 @@
         //string Name { get; set; }
         double Thickness { get; set; }
         IXShapeMaterial Material { get; set; }
+        int Priority_ { get; set; }
+        bool IsVentilated { get; set; }
     }
 }
