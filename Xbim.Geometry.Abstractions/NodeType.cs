ac86616ec0d2544d777f5da3740ae0dfaf342ed6
Steve Lockley <steve.lockley@xbim.net>
2023-01-10 10:02:59 +0000

    MaterialLayer added as a material node type

--- Xbim.Geometry.Abstractions/NodeType.cs
+++ Xbim.Geometry.Abstractions/NodeType.cs
@@ -14,6 +14,7 @@
         Material,
         MaterialConstituent,
         LayeredMaterialInstance,
+        MaterialLayer,
         ProductGroup, //IFC Assembly or IFC Group
         BRepShapePart, //Part of a shape, normally of a different colour or texture
         BRepShapeNonUniformTransformed //shape that has undergoen a nin uniform transformation (stretch)
