6a6b4de056c910f95f58741db9efaa15e49534e8
Ibrahim Saad <ibrahim.saad@xbim.net>
2023-04-05 22:37:58 +0200

    Add IXWexBimMeshFactory interface

---
+++ Xbim.Geometry.Abstractions/IXWexBimMeshFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Xbim.Geometry.Abstractions
+{
+    public interface IXWexBimMeshFactory : IXModelScoped
+    {
+        byte[] CreateWexBimMesh(IXShape shape, out IXAxisAlignedBoundingBox bounds);
+
+        byte[] CreateWexBimMesh(IXShape shape, out IXAxisAlignedBoundingBox bounds, out bool hasCurves);
+    }
+}
