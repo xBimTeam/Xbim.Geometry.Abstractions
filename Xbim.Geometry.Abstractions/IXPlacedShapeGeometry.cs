ca9a94897975b0e947c3619928ec8939f1c60ecd
Ibrahim Saad <ibrahim.saad@xbim.net>
2023-04-19 16:58:31 +0200

    fix: placed shape geometry IfcType renamed to ProductIfcType

--- Xbim.Geometry.Abstractions/IXPlacedShapeGeometry.cs
+++ Xbim.Geometry.Abstractions/IXPlacedShapeGeometry.cs
@@ -8,7 +8,7 @@
         IXShapeColour Colour { get; set; }
         IXShapeMaterialItem Material { get; set; }
         IXShape Shape { get; set; }
-        public short IfcType { get; set; }
+        public short ProductIfcType { get; set; }
         public short GeometryIfcType { get; set; }
 
     }
