6aeeed5252c1149e168b6cdea8b4a94cc385f0fc
Ibrahim Saad <ibrahim.saad@xbim.net>
2023-07-28 19:20:38 +0300

    Add IXModelPlacementBuilder interface

---
+++ Xbim.Geometry.Abstractions/IXModelPlacementBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Xbim.Ifc4.Interfaces;
+
+namespace Xbim.Geometry.Abstractions
+{
+    public interface IXModelPlacementBuilder
+    {
+        IXLocation BuildLocation(IIfcObjectPlacement placement, bool adjustWcs);
+
+        public IXLocation WorldCoordinateSystem { get; }
+    }
+}
